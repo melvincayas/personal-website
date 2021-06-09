0db885ff3b8f70c8113c1b0571c138a830f8c08a
Melvin Cayas <cayasmj@gmail.com>
2021-06-09 19:18:09 -0400

    Added links under profile picture

--- src/components/Home/Headline/ProfilePic.js
+++ src/components/Home/Headline/ProfilePic.js
@@ -8,11 +8,38 @@ const ProfilePic = () => {
 			<div
 				className={`column is-two-fifths has-text-centered px-5 ${classes["is-vertically-centered"]}`}
 			>
-				<img
-					className={`${classes.image}`}
-					src={picture}
-					alt="Melvin smiling"
-				></img>
+				<div>
+					<img
+						className={`${classes.image}`}
+						src={picture}
+						alt="Melvin smiling"
+					></img>
+					<div>
+						<i
+							className={`far fa-envelope p-2 is-size-5 ${classes.btn} ${classes.email}`}
+						></i>
+						<a
+							className={classes["github-link"]}
+							href="https://github.com/melvincayas"
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							<i
+								className={`fab fa-github mx-1 p-2 is-size-5 ${classes.btn} ${classes.github}`}
+							></i>
+						</a>
+						<a
+							className={classes["instagram-link"]}
+							href="https://www.instagram.com/melvin.cayas/"
+							target="_blank"
+							rel="noreferrer noopener"
+						>
+							<i
+								className={`fab fa-instagram p-2 is-size-5 ${classes.btn} ${classes.instagram}`}
+							></i>
+						</a>
+					</div>
+				</div>
 			</div>
 		</Fragment>
 	);
