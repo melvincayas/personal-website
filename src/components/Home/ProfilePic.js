7d076d62500681bc592252aa03682d7a3eb30721
Melvin Cayas <cayasmj@gmail.com>
2021-06-20 11:51:22 -0400

    Removed Fragments from Home and ProfilePic

--- src/components/Home/ProfilePic.js
+++ src/components/Home/ProfilePic.js
@@ -1,47 +1,44 @@
-import { Fragment } from "react";
 import picture from "../../assets/IMG_2842.jpg";
 import classes from "./styles/ProfilePic.module.css";
 
 const ProfilePic = () => {
 	return (
-		<Fragment>
-			<div
-				className={`column is-two-fifths has-text-centered px-5 ${classes["is-vertically-centered"]}`}
-			>
+		<div
+			className={`column is-two-fifths has-text-centered px-5 ${classes["is-vertically-centered"]}`}
+		>
+			<div>
+				<img
+					className={`${classes.image}`}
+					src={picture}
+					alt="Melvin smiling"
+				></img>
 				<div>
-					<img
-						className={`${classes.image}`}
-						src={picture}
-						alt="Melvin smiling"
-					></img>
-					<div>
+					<i
+						className={`far fa-envelope p-2 is-size-5 ${classes.btn} ${classes.email}`}
+					></i>
+					<a
+						className={classes["github-link"]}
+						href="https://github.com/melvincayas"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
 						<i
-							className={`far fa-envelope p-2 is-size-5 ${classes.btn} ${classes.email}`}
+							className={`fab fa-github mx-1 p-2 is-size-5 ${classes.btn} ${classes.github}`}
 						></i>
-						<a
-							className={classes["github-link"]}
-							href="https://github.com/melvincayas"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<i
-								className={`fab fa-github mx-1 p-2 is-size-5 ${classes.btn} ${classes.github}`}
-							></i>
-						</a>
-						<a
-							className={classes["instagram-link"]}
-							href="https://www.instagram.com/melvin.cayas/"
-							target="_blank"
-							rel="noreferrer noopener"
-						>
-							<i
-								className={`fab fa-instagram p-2 is-size-5 ${classes.btn} ${classes.instagram}`}
-							></i>
-						</a>
-					</div>
+					</a>
+					<a
+						className={classes["instagram-link"]}
+						href="https://www.instagram.com/melvin.cayas/"
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<i
+							className={`fab fa-instagram p-2 is-size-5 ${classes.btn} ${classes.instagram}`}
+						></i>
+					</a>
 				</div>
 			</div>
-		</Fragment>
+		</div>
 	);
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,13 @@
-import { Fragment } from "react";
 import Intro from "../components/Home/Intro";
 import ProfilePic from "../components/Home/ProfilePic";
 import classes from "./styles/Home.module.css";
 
 const Home = () => {
 	return (
-		<Fragment>
-			<div className={`columns ${classes.header}`}>
-				<Intro />
-				<ProfilePic />
-			</div>
-		</Fragment>
+		<div className={`columns ${classes.header}`}>
+			<Intro />
+			<ProfilePic />
+		</div>
 	);
 };
 
