4f1eaf6edfc2d0761d73705b08a5a205f474f72e
Melvin Cayas <cayasmj@gmail.com>
2021-06-25 22:53:26 -0400

    Fixed navbar links

--- components/UI/NavbarLinks.js
+++ components/UI/NavbarLinks.js
@@ -6,40 +6,26 @@ const NavbarLinks = () => {
 		<div className="navbar-menu" id="nav-links">
 			<div className="navbar-end">
 				<div className="navbar-item">
-					<Link
-						exact
-						className={`is-size-5 ${classes["nav-links"]}`}
-						activeClassName={classes.active}
-						href="/"
-					>
-						Home
+					<Link exact className={`is-size-5 ${classes["nav-links"]}`} href="/">
+						<a className={`is-size-5 ${classes["nav-links"]}`}>Home</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
 					<Link
 						className={`is-size-5 ${classes["nav-links"]}`}
-						activeClassName={classes.active}
 						href="/projects"
 					>
-						Projects
+						<a className={`is-size-5 ${classes["nav-links"]}`}>Projects</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
-					<Link
-						className={`is-size-5 ${classes["nav-links"]}`}
-						activeClassName={classes.active}
-						href="/resume"
-					>
-						Resume
+					<Link className={`is-size-5 ${classes["nav-links"]}`} href="/resume">
+						<a className={`is-size-5 ${classes["nav-links"]}`}>Resume</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
-					<Link
-						className={`is-size-5 ${classes["nav-links"]}`}
-						activeClassName={classes.active}
-						href="/contact"
-					>
-						Contact
+					<Link className={`is-size-5 ${classes["nav-links"]}`} href="/contact">
+						<a className={`is-size-5 ${classes["nav-links"]}`}>Contact</a>
 					</Link>
 				</div>
 			</div>
