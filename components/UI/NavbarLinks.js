3da0d1ea8ba1a214c38073444dcb7dcdaac2cff6
Melvin Cayas <cayasmj@gmail.com>
2021-06-26 11:12:47 -0400

    Added active classes for links in navbar

--- components/UI/NavbarLinks.js
+++ components/UI/NavbarLinks.js
@@ -1,28 +1,44 @@
+import { useRouter } from "next/router";
 import Link from "next/link";
 import classes from "./styles/Navbar.module.css";
 
 const NavbarLinks = () => {
+	const { pathname } = useRouter();
+
+	const isHome = pathname === "/" ? classes.active : "";
+	const isProjects = pathname === "/projects" ? classes.active : "";
+	const isResume = pathname === "/resume" ? classes.active : "";
+	const isContact = pathname === "/contact" ? classes.active : "";
+
 	return (
 		<div className="navbar-menu" id="nav-links">
 			<div className="navbar-end">
 				<div className="navbar-item">
 					<Link href="/">
-						<a className={`is-size-5 ${classes["nav-links"]}`}>Home</a>
+						<a className={`is-size-5 ${classes["nav-links"]} ${isHome}`}>
+							Home
+						</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
 					<Link href="/projects">
-						<a className={`is-size-5 ${classes["nav-links"]}`}>Projects</a>
+						<a className={`is-size-5 ${classes["nav-links"]} ${isProjects}`}>
+							Projects
+						</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
 					<Link href="/resume">
-						<a className={`is-size-5 ${classes["nav-links"]}`}>Resume</a>
+						<a className={`is-size-5 ${classes["nav-links"]} ${isResume}`}>
+							Resume
+						</a>
 					</Link>
 				</div>
 				<div className="navbar-item">
 					<Link href="/contact">
-						<a className={`is-size-5 ${classes["nav-links"]}`}>Contact</a>
+						<a className={`is-size-5 ${classes["nav-links"]} ${isContact}`}>
+							Contact
+						</a>
 					</Link>
 				</div>
 			</div>
