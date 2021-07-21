b0823faf7d47a48a4af163c94cd6e02f8376847f
Melvin Cayas <cayasmj@gmail.com>
2021-07-20 21:45:12 -0400

    Added fetching of Hashnode posts to blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,46 @@
+import { useState, useEffect } from "react";
+
+const GET_HASHNODE_POSTS = `
+query GetHashnodePosts($page: Int!) {
+	user(username: "melvincayas") {
+		publication {
+			posts(page: $page) {
+				coverImage
+				title
+				brief
+				slug
+			}
+		}
+	}
+}`;
+
 const Blog = () => {
+	const [hashnodePosts, setHashnodePosts] = useState([]);
+
+	useEffect(() => {
+		const fetchHashnodePosts = async (query, variables = {}) => {
+			const response = await fetch("https://api.hashnode.com", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					query,
+					variables,
+				}),
+			});
+
+			const result = await response.json();
+
+			setHashnodePosts(currentHashnodePosts => [
+				...currentHashnodePosts,
+				result.data.user.publication.posts,
+			]);
+		};
+
+		fetchHashnodePosts(GET_HASHNODE_POSTS, { page: 0 });
+	}, []);
+
 	return (
 		<div>
 			<h1>Filler for now</h1>
