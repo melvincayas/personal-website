cc3dcb124cd6eb62ecc58a48e10ea85e0d60cc1f
Melvin Cayas <cayasmj@gmail.com>
2021-07-20 22:16:19 -0400

    Added getStaticProps for blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,51 +1,59 @@
-import { useState, useEffect } from "react";
-
-const GET_HASHNODE_POSTS = `
-query GetHashnodePosts($page: Int!) {
-	user(username: "melvincayas") {
-		publication {
-			posts(page: $page) {
-				coverImage
-				title
-				brief
-				slug
-			}
-		}
-	}
-}`;
-
-const Blog = () => {
-	const [hashnodePosts, setHashnodePosts] = useState([]);
-
-	useEffect(() => {
-		const fetchHashnodePosts = async (query, variables = {}) => {
-			const response = await fetch("https://api.hashnode.com", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					query,
-					variables,
-				}),
-			});
-
-			const result = await response.json();
-
-			setHashnodePosts(currentHashnodePosts => [
-				...currentHashnodePosts,
-				result.data.user.publication.posts,
-			]);
-		};
-
-		fetchHashnodePosts(GET_HASHNODE_POSTS, { page: 0 });
-	}, []);
-
+const Blog = ({ hashnodePosts }) => {
 	return (
 		<div>
-			<h1>Filler for now</h1>
+			{hashnodePosts.map(post => (
+				<article key={post._id}>
+					<img src={post.coverImage} />
+					<h1>{post.title}</h1>
+					<p>{post.brief}</p>
+				</article>
+			))}
 		</div>
 	);
+};
+
+export const getStaticProps = async () => {
+	const GET_HASHNODE_POSTS = `
+		query GetHashnodePosts($page: Int!) {
+			user(username: "melvincayas") {
+				publication {
+					posts(page: $page) {
+						_id
+						coverImage
+						title
+						brief
+						slug
+					}
+				}
+			}
+}		`;
+
+	const fetchHashnodePosts = async (query, variables = {}) => {
+		const response = await fetch("https://api.hashnode.com", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				query,
+				variables,
+			}),
+		});
+
+		const result = await response.json();
+
+		return result.data.user.publication.posts;
+	};
+
+	const hashnodePosts = await fetchHashnodePosts(GET_HASHNODE_POSTS, {
+		page: 0,
+	});
+
+	return {
+		props: {
+			hashnodePosts,
+		},
+	};
 };
 
 export default Blog;
