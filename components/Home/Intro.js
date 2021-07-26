8b36cb673225aa0584255e4836427c28b03cb743
Melvin Cayas <cayasmj@gmail.com>
2021-07-26 19:03:12 -0400

    Added classes to Intro component to text

--- components/Home/Intro.js
+++ components/Home/Intro.js
@@ -1,16 +1,21 @@
+import Button from "../UI/Button";
+import introClasses from "../../styles/Home/Intro.module.css";
+
 const Intro = () => {
 	return (
-		<div>
-			<p>
+		<section>
+			<p className={introClasses.title}>
 				Hi, I'm <strong>Melvin</strong>!
 			</p>
-			<p>
+			<p className={introClasses.subtitle}>
 				I'm a <strong>full stack web developer</strong> who believes in
 				learning-by-doing and loves creating value through problem-solving. My
 				goal is to impact the world with the code I type.
 			</p>
-			<button>See My Work</button>
-		</div>
+			<Button className={introClasses["landing-page-button"]}>
+				See My Work
+			</Button>
+		</section>
 	);
 };
 
