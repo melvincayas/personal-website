aab08003b9c4f31ee198669469262857b01e74d8
Melvin Cayas <cayasmj@gmail.com>
2021-07-26 19:07:44 -0400

    Moved SocialMediaLinks into Home and moved styles to SocialMediaLinks.module.css

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import ProfilePic from "./ProfilePic";
 import Intro from "./Intro";
+import SocialMediaLinks from "./SocialMediaLinks";
 
 const Home = () => {
 	return (
 		<Fragment>
 			<ProfilePic />
 			<Intro />
+			<SocialMediaLinks />
 		</Fragment>
 	);
 };
--- components/Home/SocialMediaLinks.js
+++ components/Home/SocialMediaLinks.js
@@ -1,31 +1,31 @@
-import classes from "../../styles/Home/ProfilePic.module.css";
+import socialMediaClasses from "../../styles/Home/SocialMediaLinks.module.css";
 
 const SocialMediaLinks = () => {
 	return (
 		<div>
 			<a href="mailto:cayasmj@gmail.com?Subject=Hi,%20Melvin!">
 				<i
-					className={`far fa-envelope p-2 is-size-5 ${classes.btn} ${classes.email}`}
+					className={`far fa-envelope p-2 is-size-5 ${socialMediaClasses.btn} ${socialMediaClasses.email}`}
 				></i>
 			</a>
 			<a
-				className={classes["github-link"]}
+				className={socialMediaClasses["github-link"]}
 				href="https://github.com/melvincayas"
 				target="_blank"
 				rel="noreferrer noopener"
 			>
 				<i
-					className={`fab fa-github mx-1 p-2 is-size-5 ${classes.btn} ${classes.github}`}
+					className={`fab fa-github mx-1 p-2 is-size-5 ${socialMediaClasses.btn} ${socialMediaClasses.github}`}
 				></i>
 			</a>
 			<a
-				className={classes["instagram-link"]}
+				className={socialMediaClasses["instagram-link"]}
 				href="https://www.instagram.com/melvin.cayas/"
 				target="_blank"
 				rel="noreferrer noopener"
 			>
 				<i
-					className={`fab fa-instagram p-2 is-size-5 ${classes.btn} ${classes.instagram}`}
+					className={`fab fa-instagram p-2 is-size-5 ${socialMediaClasses.btn} ${socialMediaClasses.instagram}`}
 				></i>
 			</a>
 		</div>
