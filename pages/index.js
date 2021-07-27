38826c2f7645cbf738f0dc91dda95d216a82a13d
Melvin Cayas <cayasmj@gmail.com>
2021-07-27 18:42:12 -0400

    Added container class to HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Home from "../components/Home/Home";
+import homePageClasses from "../styles/Home/HomePage.module.css";
 
 const HomePage = () => {
 	return (
-		<div>
+		<div className={homePageClasses.container}>
 			<Home />
 		</div>
 	);
