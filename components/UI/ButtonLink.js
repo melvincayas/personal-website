a55de985d576c3704a1c01cd208be7b6d6710775
Melvin Cayas <cayasmj@gmail.com>
2021-07-28 17:29:12 -0400

    Added next/link into ButtonLink

--- components/UI/ButtonLink.js
+++ components/UI/ButtonLink.js
@@ -1,10 +1,13 @@
 import linkClasses from "../../styles/UI/ButtonLink.module.css";
+import Link from "next/link";
 
 const ButtonLink = props => {
 	return (
-		<a href={props.href} className={`${props.className} ${linkClasses.link}`}>
-			{props.children}
-		</a>
+		<Link href={props.href} passHref>
+			<a className={`${props.className} ${linkClasses.link}`}>
+				{props.children}
+			</a>
+		</Link>
 	);
 };
 
