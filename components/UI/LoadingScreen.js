d5e27f8f9607b340c13ea749708227719b3c9e3e
Melvin Cayas <cayasmj@gmail.com>
2021-07-29 19:54:24 -0400

    Created LoadingScreen with ReactLoading and position with style

--- components/UI/LoadingScreen.js
+++ components/UI/LoadingScreen.js
@@ -1,5 +1,12 @@
+import ReactLoading from "react-loading";
+import loadingScreenClasses from "../../styles/UI/LoadingScreen.module.css";
+
 const LoadingScreen = () => {
-	return <div>Sending...</div>;
+	return (
+		<div className={loadingScreenClasses.container}>
+			<ReactLoading type="spin" color="#2b95f0" />
+		</div>
+	);
 };
 
 export default LoadingScreen;
