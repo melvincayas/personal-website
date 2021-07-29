d46c02a27e00bef7db2cf6d7fbab673e47e638fc
Melvin Cayas <cayasmj@gmail.com>
2021-07-29 17:09:59 -0400

    Forgot to add title class

--- components/Contact/ContactTitle.js
+++ components/Contact/ContactTitle.js
@@ -1,7 +1,7 @@
 import contactClasses from "../../styles/Contact/Contact.module.css";
 
 const ContactTitle = props => {
-	return <h1 className={contactClasses.header}>{props.title}</h1>;
+	return <h1 className={contactClasses.title}>{props.title}</h1>;
 };
 
 export default ContactTitle;
