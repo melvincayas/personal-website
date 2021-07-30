bbf14022298944c0fdc707882f6a7fc344fd2a87
Melvin Cayas <cayasmj@gmail.com>
2021-07-30 07:20:56 -0400

    Added ReCAPTCHA component and added to Form.js

--- components/Contact/Form.js
+++ components/Contact/Form.js
@@ -4,10 +4,12 @@ import useInput from "../hooks/useInput";
 import Input from "../UI/Input";
 import ButtonLink from "../UI/ButtonLink";
 import ContactTitle from "./ContactTitle";
+import ReCAPTCHA from "react-google-recaptcha";
 import contactClasses from "../../styles/Contact/Contact.module.css";
 
 const Form = () => {
 	const [isTriedToSubmit, setIsTriedToSubmit] = useState(false);
+	const [formToken, setFormToken] = useState(null);
 	const { onFormSubmit, onFormReceived } = useContext(FormContext);
 	const {
 		enteredText: enteredName,
@@ -38,10 +40,20 @@ const Form = () => {
 		isEmpty: isMessageEmpty,
 	} = useInput();
 
+	const onCheckboxChange = token => {
+		setFormToken(token);
+	};
+
 	const formSubmitHandler = async event => {
 		event.preventDefault();
 
-		if (isNameEmpty || isEmailEmpty || isSubjectEmpty || isMessageEmpty) {
+		if (
+			isNameEmpty ||
+			isEmailEmpty ||
+			isSubjectEmpty ||
+			isMessageEmpty ||
+			!formToken
+		) {
 			setIsTriedToSubmit(true);
 			return;
 		}
@@ -53,6 +65,7 @@ const Form = () => {
 			enteredEmail,
 			enteredSubject,
 			enteredMessage,
+			formToken,
 		};
 
 		const response = await fetch("/api/contact", {
@@ -108,6 +121,12 @@ const Form = () => {
 					placeholder={isMessageError ? "Please enter a message" : "Message"}
 					className={isMessageError ? contactClasses["input-error"] : ""}
 				/>
+				<div className={contactClasses["recaptcha-container"]}>
+					<ReCAPTCHA
+						sitekey={process.env.sitekey}
+						onChange={onCheckboxChange}
+					/>
+				</div>
 				<ButtonLink type="button" className={contactClasses["contact-button"]}>
 					{isTriedToSubmit ? "Please fill everything out" : "Send Message"}
 				</ButtonLink>
