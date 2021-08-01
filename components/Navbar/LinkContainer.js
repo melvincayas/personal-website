92d546cbac500ed01757138bb50aac8dd35d4424
Melvin Cayas <cayasmj@gmail.com>
2021-08-01 15:21:49 -0400

    Renamed DesktopLinks to LinkContainer

---
+++ components/Navbar/LinkContainer.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+import NavbarLink from "./NavbarLink";
+import navbarClasses from "../../styles/UI/Navbar.module.css";
+
+const LinkContainer = props => {
+	return (
+		<ul className={navbarClasses["links-container"]}>
+			<NavbarLink onClose={props.onClose} href="/" title="Home" />
+			<NavbarLink onClose={props.onClose} href="/work" title="Work" />
+			<NavbarLink
+				onClose={props.onClose}
+				href="https://drive.google.com/file/d/1LxtZ2go30yB9UXe914lsr2bVfj6-6ZrX/view?usp=sharing"
+				title="Resume"
+			/>
+			<NavbarLink onClose={props.onClose} href="/blog" title="Blog" />
+			<NavbarLink onClose={props.onClose} href="/contact" title="Contact" />
+		</ul>
+	);
+};
+
+export default LinkContainer;
