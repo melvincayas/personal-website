fabb8b12dabb25370114f4770f1d971c3b0fbcf4
Melvin Cayas <cayasmj@gmail.com>
2021-08-01 13:20:38 -0400

    Made NavbarLink and put into Navbar

--- components/UI/Navbar.js
+++ components/UI/Navbar.js
@@ -1,17 +1,7 @@
-import NavbarBrand from "./NavbarBrand";
-import NavbarLinks from "./NavbarLinks";
-import Link from "next/link";
-import { useRouter } from "next/router";
+import NavbarLink from "./NavbarLink";
 import navbarClasses from "../../styles/UI/Navbar.module.css";
 
 const Navbar = () => {
-	const { pathname } = useRouter();
-
-	const isHome = pathname === "/" ? navbarClasses.active : "";
-	const isWork = pathname === "/work" ? navbarClasses.active : "";
-	const isBlog = pathname === "/blog" ? navbarClasses.active : "";
-	const isContact = pathname === "/contact" ? navbarClasses.active : "";
-
 	return (
 		<nav className={navbarClasses.navbar}>
 			<div className={navbarClasses["navbar-content-container"]}>
@@ -21,33 +11,14 @@ const Navbar = () => {
 					</a>
 				</div>
 				<ul className={navbarClasses["links-container"]}>
-					<li className={navbarClasses.link}>
-						<Link href="/">
-							<a className={`${isHome}`}>Home</a>
-						</Link>
-					</li>
-					<li className={navbarClasses.link}>
-						<Link href="/work">
-							<a className={`${isWork}`}>Work</a>
-						</Link>
-					</li>
-					<li className={navbarClasses.link}>
-						<Link href="https://drive.google.com/file/d/1LxtZ2go30yB9UXe914lsr2bVfj6-6ZrX/view?usp=sharing">
-							<a target="_blank" rel="noreferrer noopener">
-								Resume
-							</a>
-						</Link>
-					</li>
-					<li className={navbarClasses.link}>
-						<Link href="/blog">
-							<a className={`${isBlog}`}>Blog</a>
-						</Link>
-					</li>
-					<li className={navbarClasses.link}>
-						<Link href="/contact">
-							<a className={`${isContact}`}>Contact</a>
-						</Link>
-					</li>
+					<NavbarLink href="/" title="Home" />
+					<NavbarLink href="/work" title="Work" />
+					<NavbarLink
+						href="https://drive.google.com/file/d/1LxtZ2go30yB9UXe914lsr2bVfj6-6ZrX/view?usp=sharing"
+						title="Resume"
+					/>
+					<NavbarLink href="/blog" title="Blog" />
+					<NavbarLink href="/contact" title="Contact" />
 				</ul>
 			</div>
 		</nav>
---
+++ components/UI/NavbarLink.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import navbarClasses from "../../styles/UI/Navbar.module.css";
+
+const NavbarLink = props => {
+	const { pathname } = useRouter();
+
+	const isActive = pathname === props.href ? navbarClasses.active : "";
+
+	return (
+		<li className={navbarClasses.link}>
+			<Link href={props.href}>
+				<a
+					target={props.title === "Resume" ? "_blank" : ""}
+					rel={props.title === "Resume" ? "noreferrer noopener" : ""}
+					className={`${isActive}`}
+				>
+					{props.title}
+				</a>
+			</Link>
+		</li>
+	);
+};
+
+export default NavbarLink;
