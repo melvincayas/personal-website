7f5edbb824c85d9b4d18fe1c6d83f5249ae11680
Melvin Cayas <cayasmj@gmail.com>
2021-08-03 12:33:55 -0400

    Deleted old files

--- public/scripts/navbar.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener("DOMContentLoaded", () => {
-// 	const burgerIcon = document.getElementById("burger");
-// 	const navbarMenu = document.getElementById("nav-links");
-
-// 	burgerIcon.addEventListener("click", () => {
-// 		navbarMenu.classList.toggle("is-active");
-// 		burgerIcon.classList.toggle("is-active");
-// 	});
-// });
