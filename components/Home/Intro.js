15f39f68a0386df217faff0dcdb5696ac97571c7
Melvin Cayas <cayasmj@gmail.com>
2021-08-03 23:16:07 -0400

    Removed extra quotes from Intro

--- components/Home/Intro.js
+++ components/Home/Intro.js
@@ -6,10 +6,10 @@ const Intro = () => {
 		<section className={introClasses["section-container"]}>
 			<div className={introClasses["text-container"]}>
 				<p className={introClasses.title}>
-					Hi, I`&apos;`m <strong>Melvin</strong>!
+					Hi, I&apos;m <strong>Melvin</strong>!
 				</p>
 				<p className={introClasses.subtitle}>
-					I`&apos;`m a <strong>full stack web developer</strong> who believes in
+					I&apos;m a <strong>full stack web developer</strong> who believes in
 					learning-by-doing and loves creating value through problem-solving. My
 					goal is to impact the world with the code I type.
 				</p>
