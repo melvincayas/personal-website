ba4f3ccc91628f1f0bac12ebefa5e68f54624395
Melvin Cayas <cayasmj@gmail.com>
2021-08-19 21:07:51 -0400

    Updated HTML semantics in Intro.js.

--- components/Home/Intro.js
+++ components/Home/Intro.js
@@ -4,16 +4,16 @@ import introClasses from "../../styles/Home/Intro.module.css";
 const Intro = () => {
 	return (
 		<section className={introClasses["section-container"]}>
-			<div className={introClasses["text-container"]}>
-				<p className={introClasses.title}>
+			<article className={introClasses["text-container"]}>
+				<header className={introClasses.title}>
 					Hi, I&apos;m <strong>Melvin</strong>!
-				</p>
+				</header>
 				<p className={introClasses.subtitle}>
 					I&apos;m a <strong>full stack web developer</strong> who believes in
 					learning-by-doing and loves creating value through problem-solving. My
 					goal is to impact the world with the code I type.
 				</p>
-			</div>
+			</article>
 			<ButtonLink href="/work" className={introClasses["landing-page-button"]}>
 				See My Work
 			</ButtonLink>
