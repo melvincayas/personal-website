c800d18993df1a5c94bcc24935cf2eb400b120d6
Melvin Cayas <cayasmj@gmail.com>
2021-08-19 21:03:53 -0400

    Added personal website to Work page

--- public/data/work-data.js
+++ public/data/work-data.js
@@ -1,6 +1,15 @@
 export const projects = [
 	{
 		id: 1,
+		title: "Personal Website",
+		content:
+			"The website you're currently on! My mobile-friendly personal page to showcase my work and blog. Feel free to fork the repo and use it as a template for your own page!",
+		live: "https://melvincayas.com",
+		github: "https://github.com/melvincayas/personal-website",
+		tags: ["HTML", "CSS", "React", "Next.js", "Bulma"],
+	},
+	{
+		id: 2,
 		title: "Math Whizard",
 		content:
 			"Math game allowing users to customize the difficulty and the operators used. Two game modes are available: infinite questions or a 60 second time attack.",
@@ -9,16 +18,24 @@ export const projects = [
 		tags: ["HTML", "CSS", "JavaScript"],
 	},
 	{
-		id: 2,
+		id: 3,
 		title: "Poster",
 		content:
 			"Social media website with user authentication and liking, saving and commenting on posts. Users can follow others to curate their timeline.",
 		live: "https://my-poster-app.herokuapp.com/",
 		github: "https://github.com/melvincayas/Poster-App",
-		tags: ["HTML", "CSS", "JavaScript", "EJS", "Node.js", "MongoDB"],
+		tags: [
+			"HTML",
+			"CSS",
+			"JavaScript",
+			"EJS",
+			"Node.js",
+			"MongoDB",
+			"Bootstrap",
+		],
 	},
 	{
-		id: 3,
+		id: 4,
 		title: "Surely Noted",
 		content:
 			"Currently on-going project with user authentication for creating, sharing, and leaving notes on To-Do lists.",
